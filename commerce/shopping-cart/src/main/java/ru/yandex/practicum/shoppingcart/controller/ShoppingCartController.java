8f51274a51bf2c57c46db56cba314644b4f8ade7
Ilya Kravchenko <kravchi0803@gmail.com>
2025-09-01 23:42:40 +0300

    logging

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/shoppingcart/controller/ShoppingCartController.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/shoppingcart/controller/ShoppingCartController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.shoppingcart.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/shopping-cart")
 @RequiredArgsConstructor
@@ -27,29 +29,34 @@ public class ShoppingCartController implements ShoppingCartClient {
 
     @GetMapping
     public ShoppingCartDto getCart(@RequestParam String username) {
+        log.info("GET /api/v1/shopping-cart username={}", username);
         return service.getCart(username);
     }
 
     @PutMapping
     public ShoppingCartDto addProduct(@RequestParam String username,
                                       @RequestBody Map<UUID, Long> productsToAdd) {
+        log.info("PUT /api/v1/shopping-cart username={} products={}", username, productsToAdd);
         return service.addProducts(username, productsToAdd);
     }
 
     @PostMapping("/remove")
     public ShoppingCartDto removeProducts(@RequestParam String username,
                                           @RequestBody List<UUID> productIds) {
+        log.info("POST /api/v1/shopping-cart/remove username={} productIds={}", username, productIds);
         return service.removeProducts(username, productIds);
     }
 
     @PostMapping("/change-quantity")
     public ShoppingCartDto changeQuantity(@RequestParam String username,
                                           @RequestBody ChangeProductQuantityRequest request) {
+        log.info("POST /api/v1/shopping-cart/change-quantity username={} request={}", username, request);
         return service.changeQuantity(username, request);
     }
 
     @DeleteMapping
     public void deactivateCart(@RequestParam String username) {
+        log.info("DELETE /api/v1/shopping-cart username={}", username);
         service.deactivateCart(username);
     }
 }
